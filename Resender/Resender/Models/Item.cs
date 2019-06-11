64718fb0c45643aea2218687a1bbab35dc589d7a
Anton Mitsengendler <a.mic@rtc.ru>
2019-06-11 09:53:34 +0300

    Add project files.

---
+++ Resender/Resender/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Resender.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Phone { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ Resender/Resender/Services/MockDataStore.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Resender.Models;
+
+namespace Resender.Services
+{
+    public class MockDataStore : IDataStore<Item>
+    {
+        List<Item> items;
+
+        public MockDataStore()
+        {
+            items = new List<Item>();
+            var mockItems = new List<Item>
+            {
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Test message", Phone="+79214065172" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "=Date", Phone="+79211234578" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Yet another test", Phone="+79210000000" }
+            };
+
+            foreach (var item in mockItems)
+            {
+                items.Add(item);
+            }
+        }
+
+        public async Task<bool> AddItemAsync(Item item)
+        {
+            items.Add(item);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> UpdateItemAsync(Item item)
+        {
+            var oldItem = items.Where((Item arg) => arg.Id == item.Id).FirstOrDefault();
+            items.Remove(oldItem);
+            items.Add(item);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> DeleteItemAsync(string id)
+        {
+            var oldItem = items.Where((Item arg) => arg.Id == id).FirstOrDefault();
+            items.Remove(oldItem);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<Item> GetItemAsync(string id)
+        {
+            return await Task.FromResult(items.FirstOrDefault(s => s.Id == id));
+        }
+
+        public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return await Task.FromResult(items);
+        }
+    }
+}
