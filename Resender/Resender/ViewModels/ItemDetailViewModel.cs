11d4f41e0c1c93e9a1fea1baefa024dfecc97a52
Anton Mitsengendler <a.mic@rtc.ru>
2019-06-13 00:44:28 +0300

    base sms functionality

--- Resender/Resender/ViewModels/ItemDetailViewModel.cs
+++ Resender/Resender/ViewModels/ItemDetailViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Threading.Tasks;
+using Plugin.Messaging;
 using Resender.Models;
 using Xamarin.Forms;
 
@@ -8,10 +9,23 @@ namespace Resender.ViewModels
     public class ItemDetailViewModel : BaseViewModel
     {
         public Item Item { get; set; }
+        public Command SendMessageCommand { get; private set; }
         public ItemDetailViewModel(Item item = null)
         {
             Title = item?.Name;
             Item = item;
+            SendMessageCommand = new Command(ExecuteSendMessage);
+        }
+
+        private async void ExecuteSendMessage()
+        {
+            await Task.Run(() =>
+                {
+                    var smsMessenger = CrossMessaging.Current.SmsMessenger;
+                    if (smsMessenger.CanSendSmsInBackground)
+                        smsMessenger.SendSmsInBackground(Item.Phone, Item.Text);
+                }
+            );
         }
 
         public async void DeleteCurrentItem()
